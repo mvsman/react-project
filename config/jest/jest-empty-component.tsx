6f26c35cc8e0e44e0761f65fccebb51b99a78e74
Vladimir <mvsman8@gmail.com>
2023-03-15 12:03:46 +0500

    loki tests

--- config/jest/jest-empty-component.tsx
+++ config/jest/jest-empty-component.tsx
@@ -1,10 +1,3 @@
-import React from 'react';
-
-// eslint-disable-next-line func-names
-// const jestEmptyCompoent = function () {
-//   return <div />;
-// };
-
 const jestEmptyCompoent = () => <div />;
 
 export default jestEmptyCompoent;
